aae9219b75cd1322cb7802343adf7905569c190c
jakyastik <12696342+jakyastik@users.noreply.github.com>
2024-05-29 02:56:51 +0530

    calculator functions working except operator right after result

--- script.js
+++ script.js
@@ -1,21 +1,114 @@
-function add(x, y) {
-	return x + y;
+function add(firstNum, lastNum) {
+	return firstNum + lastNum;
 }
 
-function subtrack(x, y) {
-	return x - y;
+function substract(firstNum, lastNum) {
+	return firstNum - lastNum;
 }
 
-function multiply(x, y) {
-	return x * y;
+function multiply(firstNum, lastNum) {
+	return firstNum * lastNum;
 }
 
-function divide(x, y) {
-	return x / y;
+function divide(firstNum, lastNum) {
+	return firstNum / lastNum;
 }
 
-let firstNum;
+let stream = [];
+let firstNumArray = [];
+let lastNumArray = [];
+let firstNum = 0;
+let lastNum = 0;
 let operator;
-let lastNum;
+let result;
+let inputBox;
 
-function operate(firstNum, operate, lastNum) {}
+let numberButton = document.querySelectorAll(".numberButton");
+let displayDiv = document.querySelector(".displayDiv");
+
+numberButton.forEach((numberButton) => {
+	numberButton.addEventListener("click", (event) => {
+		let pressedValue = event.target.value;
+
+		if (operator) {
+			lastNumArray.push(pressedValue);
+			lastNum = lastNumArray.join("");
+			displayDiv.innerHTML = lastNum;
+			console.log("Last num is ", lastNum);
+		} else {
+			firstNumArray.push(pressedValue);
+			firstNum = firstNumArray.join("");
+			displayDiv.innerHTML = firstNum;
+			console.log("First num is ", firstNum);
+		}
+	});
+});
+
+let operatorButton = document.querySelectorAll(".opButton");
+
+operatorButton.forEach((operatorButton) => {
+	operatorButton.addEventListener("click", (event) => {
+		let pressedValue = event.target.value;
+		operator = pressedValue;
+		displayDiv.innerHTML = operator;
+
+		let toFirstNum = Number(firstNum);
+		let toLastNum = Number(lastNum);
+
+		if ((firstNum, lastNum)) {
+			if (operator == "+") {
+				result = add(toFirstNum, toLastNum);
+			} else if (operator == "-") {
+				result = substract(toFirstNum, toLastNum);
+			} else if (operator == "*") {
+				result = multiply(toFirstNum, toLastNum);
+			} else if (operator == "/") {
+				result = divide(toFirstNum, toLastNum);
+			}
+		} else if (firstNum == 0 || lastNum == 0) {
+			if (operator == "+") {
+				result = add(toFirstNum, toLastNum);
+			} else if (operator == "-") {
+				result = substract(toFirstNum, toLastNum);
+			} else if (operator == "*") {
+				result = multiply(toFirstNum, toLastNum);
+			} else if (operator == "/") {
+				result = divide(toFirstNum, toLastNum);
+			}
+		}
+	});
+});
+
+let resultButton = document.querySelector(".resultButton");
+resultButton.addEventListener("click", () => {
+	let toFirstNum = Number(firstNum);
+	let toLastNum = Number(lastNum);
+
+	if (operator == "+") {
+		result = add(toFirstNum, toLastNum);
+	} else if (operator == "-") {
+		result = substract(toFirstNum, toLastNum);
+	} else if (operator == "*") {
+		result = multiply(toFirstNum, toLastNum);
+	} else if (operator == "/") {
+		result = divide(toFirstNum, toLastNum);
+	}
+
+	displayDiv.innerHTML = result;
+	firstNumArray = [];
+	firstNumArray.push(result);
+	firstNum = firstNumArray.join();
+	lastNumArray = [];
+	lastNum = firstNumArray;
+	console.log(firstNum, firstNumArray, lastNum, lastNumArray);
+});
+
+function clearDiv() {
+	firstNumArray = [];
+	lastNumArray = [];
+	lastNum = 0;
+	firstNum = 0;
+	displayDiv.innerHTML = "";
+	operator = "";
+	console.clear();
+}
