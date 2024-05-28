b88108c427f15b77e61e1bde2f2b2fac27f91f48
jakyastik <12696342+jakyastik@users.noreply.github.com>
2024-05-28 14:18:14 +0530

    created primary calculator layout

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(x, y) {
+	return x + y;
+}
+
+function subtrack(x, y) {
+	return x - y;
+}
+
+function multiply(x, y) {
+	return x * y;
+}
+
+function divide(x, y) {
+	return x / y;
+}
+
+let firstNum;
+let operator;
+let lastNum;
+
+function operate(firstNum, operate, lastNum) {}
